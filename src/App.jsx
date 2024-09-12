a12533bf45bcfad136ccc57235b03276f0f92769
Matheus Silva <mate.almeidasilva@gmail.com>
2024-09-12 20:28:23 -0300

    making the dark and light mode button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import { useState } from 'react'
 import './App.css'
 import NavBar from './components/NavBar/NavBar'
 
 function App() {
 
+  const [theme, setTheme] = useState('dark')
 
   return (
-    <>
-     <NavBar/>
-    </>
+    <div className={`container ${theme}`}> 
+
+     <NavBar theme={theme} setTheme={setTheme}/>
+    </div>
+    
   )
 }
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,7 +3,12 @@ import "./NavBar.css";
 import logo from "../../assets/logo-senai.png";
 import { FaMoon, FaSun } from "react-icons/fa";
 
-const NavBar = () => {
+const NavBar = ({theme, setTheme}) => {
+
+const toggle_mode = () =>{
+  theme == 'dark' ? setTheme('light') : setTheme('dark')
+}
+
   return (
     <nav className="navbar">
       <div className="logo">
@@ -11,8 +16,8 @@ const NavBar = () => {
         <p className="line">|</p>
         <p className="text">Portal de Vagas</p>
       </div>
-      <div className="theme-button">
-        <FaSun />
+      <div className="theme-button" onClick={()=>toggle_mode()}>
+        {theme == "dark"? <FaSun size={30}/> : <FaMoon size={30}/>}
       </div>
     </nav>
   );
