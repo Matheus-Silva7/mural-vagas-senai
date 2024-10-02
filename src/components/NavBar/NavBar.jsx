2bbf96ffec9047a9c3db97061f796ae3d66e9cb1
Matheus-Silva7 <mate.almeidasilva@gmail.com>
2024-10-02 01:02:52 -0300

    navbar responsive

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./NavBar.css";
-import ToogleMode from "../Buttons/ToogleMode/ToggleMode"
+import ToogleMode from "../Buttons/ToogleMode/ToggleMode";
 import Logo from "../Logo/Logo";
-
+import { IoMenu } from "react-icons/io5";
+import { IoMdClose } from "react-icons/io";
 
 const NavBar = ({ theme, setTheme }) => {
+  const [active, setActive] = useState(false);
+
+  const clickMenu = () => {
+    setActive(!active);
+  };
+
+  useEffect(() => {
+    if (active) {
+      document.body.classList.add("no-scroll");
+    } else {
+      document.body.classList.remove("no-scroll");
+    }
+  }, [active]);
 
   return (
     <nav className="navbar">
       <Logo />
-      <div className="right-side">
+      <div className={`right-side ${active ? "active" : ""}`}>
         <ul>
           <li>Vagas</li>
           <li>Dúvidas</li>
         </ul>
         <ToogleMode setTheme={setTheme} theme={theme} />
+      </div>
+      <div className="menu">
+        {active ? (
+          <IoMdClose size={40} onClick={clickMenu} />
+        ) : (
+          <IoMenu size={40} onClick={clickMenu} />
+        )}
       </div>
     </nav>
   );
--- src/components/NavBar/NavBarEmpresa.jsx
+++ src/components/NavBar/NavBarEmpresa.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import "./NavBar.css";
-import ToogleMode from "../Buttons/ToogleMode/ToggleMode"
+import ToogleMode from "../Buttons/ToogleMode/ToggleMode";
 import Logo from "../Logo/Logo";
 import { Link } from 'react-router-dom';
+import { IoMdClose } from 'react-icons/io';
+import { IoMenu } from 'react-icons/io5';
 
 const NavBarEmpresa = ({ theme, setTheme }) => {
+
+  const [active, setActive] = useState(false);
+
+  const clickMenu = () => {
+    setActive(!active);
+  };
+
+  useEffect(() => {
+    if (active) {
+      document.body.classList.add("no-scroll");
+    } else {
+      document.body.classList.remove("no-scroll");
+    }
+
+    return () => {
+      document.body.classList.remove("no-scroll");
+    };
+  }, [active]);
+
   return (
     <nav className="navbar">
       <Logo />
-      <div className="right-side">
+      <div className={`right-side ${active ? "active" : ""}`}>
         <ul>
           <li><Link to={"/empresa"}>Minhas vagas</Link></li>
           <li><Link to={"/empresa/dados"}>Meus Dados</Link></li>
@@ -16,8 +37,15 @@ const NavBarEmpresa = ({ theme, setTheme }) => {
         </ul>
         <ToogleMode setTheme={setTheme} theme={theme} />
       </div>
+      <div className="menu">
+        {active ? (
+          <IoMdClose size={40} onClick={clickMenu} />
+        ) : (
+          <IoMenu size={40} onClick={clickMenu} />
+        )}
+      </div>
     </nav>
   );
-}
+};
 
-export default NavBarEmpresa
+export default NavBarEmpresa;
